7f06bca38153008d0ae1605f2986d75f7e8566ec
Nyhn <37303530+Nyhn@users.noreply.github.com>
2020-04-12 20:13:35 +0200

    Create SiteRepository.java

---
+++ src/main/java/com/oc/Climb/DAO/SiteRepository.java
@@ -0,0 +1,7 @@
+package com.oc.Climb.DAO;
+
+import com.oc.Climb.model.Site;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiteRepository extends JpaRepository<Site,Long> {
+}
