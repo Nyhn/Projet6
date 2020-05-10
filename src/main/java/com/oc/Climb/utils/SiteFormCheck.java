8bd0e0e46f88940f9c7bd517da8cccb2007d68e9
Nyhn <37303530+Nyhn@users.noreply.github.com>
2020-05-10 20:24:12 +0200

    ajout d'information dans le formulaire

--- src/main/java/com/oc/Climb/utils/SiteFormCheck.java
+++ src/main/java/com/oc/Climb/utils/SiteFormCheck.java
@@ -1,4 +1,8 @@
 package com.oc.Climb.utils;
 
 public class SiteFormCheck {
+    private boolean nameSize;
+    private boolean nameEmpty;
+    private boolean placeSize;
+    private boolean placeEmpty;
 }
